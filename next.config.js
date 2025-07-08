d1b96348e4ac2721ae2aebec8f19f7a2e1a24dd8
Yamato Tsugawa <tetsugakuman@gmail.com>
2025-07-08 09:38:54 +0900

    dummy update to force clean build

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+const path = require('path');
+
+module.exports = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
