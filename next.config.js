6d27d135d37a923e81de752737ad3fa21cd80adc
Yamato Tsugawa <tetsugakuman@gmail.com>
2025-07-08 15:04:37 +0900

    Remove swcMinify option from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // 修正点: swcMinifyオプションを削除 (Next.js 15ではデフォルトで有効なため不要)
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     config.resolve.alias['@lib'] = path.resolve(__dirname, 'src/lib');
